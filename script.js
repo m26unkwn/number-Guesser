dc5aa630de1dbc1c6492cebc183b45d54171ed02
m26unkwn <shukla9915294376@gmail.com>
2021-02-09 11:11:52 +0530

    Added few functions

--- script.js
+++ script.js
@@ -2,5 +2,22 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Generate random number between 0 to 10
 
+const generateTarget=()=> Math.floor(Math.random() * 10)
+//generateTarget()
+
+//compareGuesser 
+
+const compareGuesses=(userInput , computerInput, targetNumber)=>{
+    if ((Math.abs(userInput,targetNumber)) === (Math.abs(computerInput,targetNumber))){
+        return true
+    }
+    if ((Math.abs(userInput,targetNumber)) > (Math.abs(computerInput,targetNumber))){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
